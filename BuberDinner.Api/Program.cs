ee60ca632f1b5716505748ac62a2d6eb688b85e1
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-07-25 14:38:50 +0200

    Add auth controller and services and DI bluprint

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,22 +1,26 @@
+using BuberDinner.Application;
+using BuberDinner.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddApplication().AddInfrastructure();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
+var app = builder.Build();
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    app.Run();
+}
+
