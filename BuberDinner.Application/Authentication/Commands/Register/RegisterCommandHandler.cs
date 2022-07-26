10823efe559c4ac6888ae72b4fb9503273af97c0
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-07-26 15:33:20 +0200

    Implement CQRS and add  MediatR

---
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler :
+    IRequestHandler<RegisterCommand, ErrorOr<AuthenticationResult>>
+{
+    #region Fields :
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+    #endregion
+
+    #region CTORS :
+    public RegisterCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+    #endregion
+
+    #region Operations :
+    public Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        //1.Validate the user doesn;t exist
+        if (_userRepository.GetUserByEmail(request.Email) is not null)
+            return Task.FromResult<ErrorOr<AuthenticationResult>>(Errors.User.DuplicateEmail);
+        //2.Create user (generate unique ID)
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Password = request.Password
+        };
+        _userRepository.AddUser(user);
+        //Create JWT
+        var token = _jwtTokenGenerator.GenerateToken(user);
+        return Task.FromResult<ErrorOr<AuthenticationResult>>(new AuthenticationResult(user, token));
+    }
+    #endregion
+}
---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Queries.Login;
+
+public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+{
+    #region Fields :
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+    #endregion
+
+    #region CTORS :
+    public LoginQueryHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+    #endregion
+    #region Operations :
+    public Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery request, CancellationToken cancellationToken)
+    {
+        //1.Validate the user exists
+        if (_userRepository.GetUserByEmail(request.Email) is not User user)
+            return Task.FromResult<ErrorOr<AuthenticationResult>>(Errors.Authentication.InvalidCredentials);
+        //2.Validate the password is correct
+        if (user.Password != request.Password)
+            return Task.FromResult<ErrorOr<AuthenticationResult>>(Errors.Authentication.InvalidCredentials);
+        //3.Create Jwt
+        var token = _jwtTokenGenerator.GenerateToken(user);
+        return Task.FromResult<ErrorOr<AuthenticationResult>>(new AuthenticationResult(user, token));
+    }
+    #endregion
+}
